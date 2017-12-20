6a8debfb4c0e8b12580b01e1999ecf9afaaba602
AndresEstan <andrestan1995@gmail.com>
2017-12-19 19:42:10 -0500

    interfaces

---
+++ app/src/main/java/com/example/estan/censodecarga/Aparato.java
@@ -0,0 +1,41 @@
+package com.example.estan.censodecarga;
+
+/**
+ * Created by Estan on 19/12/2017.
+ */
+
+public class Aparato {
+private String nombre;
+private int cantidad;
+private int total;
+
+    public Aparato(String nombre, int cantidad, int total) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.total = total;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+}
---
+++ app/src/main/java/com/example/estan/censodecarga/Metodos.java
@@ -0,0 +1,9 @@
+package com.example.estan.censodecarga;
+
+/**
+ * Created by Estan on 19/12/2017.
+ */
+
+public class Metodos {
+
+}
