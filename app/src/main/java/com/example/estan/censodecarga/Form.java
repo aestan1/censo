de6ae53abd3dc2769e6cd7ab699c3cd0e6570d34
AndresEstan <andrestan1995@gmail.com>
2017-12-20 15:36:45 -0500

    crea xls nuevos

--- app/src/main/java/com/example/estan/censodecarga/Form.java
+++ app/src/main/java/com/example/estan/censodecarga/Form.java
@@ -1,13 +1,77 @@
 package com.example.estan.censodecarga;
 
+import android.database.Cursor;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import jxl.Workbook;
+import jxl.WorkbookSettings;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
 
 public class Form extends AppCompatActivity {
-
+    private ArrayList<Aparato> aparatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        aparatos=Datos.obtener();
+
+
+
+
+
+        File sd = Environment.getExternalStorageDirectory();
+        String csvFile = "formatoCenso.xls";
+
+        File directory = new File(sd.getAbsolutePath());
+        //create directory if not exist
+        if (!directory.isDirectory()) {
+            directory.mkdirs();
+        }
+        try {
+
+            //file path
+            File file = new File(directory, csvFile);
+            WorkbookSettings wbSettings = new WorkbookSettings();
+            wbSettings.setLocale(new Locale("en", "EN"));
+            WritableWorkbook workbook;
+            workbook = Workbook.createWorkbook(file, wbSettings);
+            //Excel sheet name. 0 represents first sheet
+            WritableSheet sheet = workbook.createSheet("userList", 0);
+
+            sheet.addCell(new Label(0, 0, "Item")); // column and row
+            sheet.addCell(new Label(1, 0, "Cantidad"));
+
+
+            for (int i = 0; i < aparatos.size() ; i++) {
+
+                sheet.addCell(new Label(3, 11, ""+aparatos.get(i).getCantidad()));
+
+            }
+
+
+            //closing cursor
+
+            workbook.write();
+
+            workbook.close();
+            Toast.makeText(getApplication(), "Data Exported in a Excel Sheet", Toast.LENGTH_SHORT).show();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
+
+
 }
